c32327364642e840527ac9a19b0e5a27249082a7
Krys <krysbaum@gmail.com>
2024-02-26 16:40:36 -0800

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.create(name: "NuPhone", price: 1200, image_url: nil, description: "This is the phone that will take you to the future.")
+Product.create(name: "CoM9U70R", price: 2200, image_url: nil, description: "This is the only laptop you'll ever need.")
+Product.create(name: "Zubz", price: 750, image_url: nil, description: "The greatest sounds headphones have to offer.")
