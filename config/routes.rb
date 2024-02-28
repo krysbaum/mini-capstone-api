40e2332b1aed5925b3432d3ddb37106e7cc18ff7
Krys <krysbaum@gmail.com>
2024-02-27 18:04:44 -0800

    Add patch method, update tests

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
   post "/products" => "products#create"
   get "/products/:id" => "products#show"
+  patch "/products/:id" => "products#update"
 end
