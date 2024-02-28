81c4d448766ade0686adba63dcac27a4f19ac238
Krys <krysbaum@gmail.com>
2024-02-27 17:24:46 -0800

    Add tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/products.json"
+    assert_response 200
+  end
+
+  test "show" do
+    get "/products/#{Product.first.id}.json"
+    assert_response 200
+  end
+
+  test "create" do
+    post "/products.json"
+    assert_response 200
+  end
 end
