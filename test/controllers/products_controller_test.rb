6fc91ce4c71ee01dd7d84d3f60195ee08d93da87
Krys <krysbaum@gmail.com>
2024-03-03 08:03:52 -0800

    Update tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -11,8 +11,18 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { price: 1, name: "test product", description: "test description" }
+      post "/products.json", params: { price: 1, name: "test product", description: "test description", image_url: "image.jpg" }
+      data = JSON.parse(response.body)
       assert_response 200
+      refute_nil data["id"]
+      assert_equal "test product", data["name"]
+      assert_equal "1.0", data["price"]
+      assert_equal "image.jpg", data["image_url"]
+      assert_equal "test description", data["description"]
+    end
+    assert_difference "Product.count", 0 do
+      post "/products.json", params: {}
+      assert_response 422
     end
   end
 
@@ -31,6 +41,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
+    assert_equal product.price.to_s, data["price"]
+    assert_equal product.image_url, data["image_url"]
+    assert_equal product.description, data["description"]
+
+    patch "/products/#{product.id}.json", params: { price: -100 }
+    assert_response 422
   end
 
   test "destroy" do
