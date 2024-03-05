acae54c10b02ada042c55cfce371a53193a75bb7
Krys <krysbaum@gmail.com>
2024-03-04 16:34:02 -0800

    Add Supplier model; update seed file. Update inventory and supplier_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,10 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Product.create(name: "NuPhone", price: 1200, image_url: "https://thumbs.dreamstime.com/b/futuriatic-transparent-glass-phone-technology-concept-high-temperature-weather-forecast-sunny-day-icon-show-screen-222585873.jpg", description: "Take yourself into the future.")
-Product.create(name: "CoM9U70R", price: 2200, image_url: "https://cdn.mos.cms.futurecdn.net/4mfNfwcK83FcRprS2yeMoK-1920-80.jpg.webp", description: "The only laptop you'll ever need.")
-Product.create(name: "Zubz", price: 750, image_url: "https://cdn.vox-cdn.com/thumbor/7eUJVmyvhCj2rckyiOg_xEFeRrQ=/0x0:2040x1351/1400x1050/filters:focal(1020x675:1021x676)/cdn.vox-cdn.com/uploads/chorus_asset/file/15729086/sony-future-lab-project-n-sxsw-2016-2.0.0.1457894980.jpg", description: "The greatest sounds headphones have to offer.")
-Product.create(name: "The Scr33n", price: 550, image_url: "https://www.yankodesign.com/images/design_news/2018/04/transparent_television_layout.jpg", description: "Expand your horizons.")
+Product.create(name: "NuPhone", price: 1200, image_url: "https://thumbs.dreamstime.com/b/futuriatic-transparent-glass-phone-technology-concept-high-temperature-weather-forecast-sunny-day-icon-show-screen-222585873.jpg", description: "Take yourself into the future.", inventory: 6, supplier_id: 1)
+Product.create(name: "CoM9U70R", price: 2200, image_url: "https://cdn.mos.cms.futurecdn.net/4mfNfwcK83FcRprS2yeMoK-1920-80.jpg.webp", description: "The only laptop you'll ever need.", inventory: 3, supplier_id: 1)
+Product.create(name: "Zubz", price: 750, image_url: "https://cdn.vox-cdn.com/thumbor/7eUJVmyvhCj2rckyiOg_xEFeRrQ=/0x0:2040x1351/1400x1050/filters:focal(1020x675:1021x676)/cdn.vox-cdn.com/uploads/chorus_asset/file/15729086/sony-future-lab-project-n-sxsw-2016-2.0.0.1457894980.jpg", description: "The greatest sounds headphones have to offer.", inventory: 13, supplier_id: 2)
+Product.create(name: "The Scr33n", price: 550, image_url: "https://www.yankodesign.com/images/design_news/2018/04/transparent_television_layout.jpg", description: "Expand your horizons.", inventory: 9, supplier_id: 1)
+
+Supplier.create(name: "L3m0n Inc.", email: "admin@l3m0nco.com", phone_number: "888-444-5555 ext: 1")
+Supplier.create(name: "Ndr!ve Inc.", email: "hello@ndrive.com", phone_number: "999-333-2222")
