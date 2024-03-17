7785f1801459ee109f60e610c371abf045dff0ac
Krys <krysbaum@gmail.com>
2024-03-17 07:33:13 -0700

    Update cors to allow requests from localhost

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+Supplier.create!(name: "L3m0n Inc.", email: "lemons@example.com", phone_number: "888-444-5555 ext: 1")
+Supplier.create!(name: "Ndr!ve Inc.", email: "ndrive@example.com", phone_number: "999-333-2222")
+
 Product.create!(name: "NuPhone", price: 1200, description: "Take yourself into the future.", inventory: 6, supplier_id: 1)
 Product.create!(name: "CoM9U70R", price: 2200, description: "The only laptop you'll ever need.", inventory: 3, supplier_id: 1)
 Product.create!(name: "Zubz", price: 750, description: "The greatest sounds headphones have to offer.", inventory: 13, supplier_id: 2)
 Product.create!(name: "The Scr33n", price: 550, description: "Expand your horizons.", inventory: 9, supplier_id: 1)
-
-Supplier.create!(name: "L3m0n Inc.", email: "lemons@example.com", phone_number: "888-444-5555 ext: 1")
-Supplier.create!(name: "Ndr!ve Inc.", email: "ndrive@example.com", phone_number: "999-333-2222")
 
 Image.create!(url: "https://thumbs.dreamstime.com/b/futuriatic-transparent-glass-phone-technology-concept-high-temperature-weather-forecast-sunny-day-icon-show-screen-222585873.jpg", product_id: 1)
 Image.create!(url: "https://cdn.mos.cms.futurecdn.net/4mfNfwcK83FcRprS2yeMoK-1920-80.jpg.webp", product_id: 2)
