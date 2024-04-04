1b942ad41093e97424db399107e067766a39350d
Krys <krysbaum@gmail.com>
2024-04-03 18:54:12 -0700

    I am dumb.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone.peterxjang.com", "localhost:5173", "mini-capstone-api-lvcl.onrender.com"
+    origins "mini-capstone.peterxjang.com", "localhost:5173", "mini-capstone-frontend-vw13.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
